db60265911210f81f82d11f46aa6593346af7e67
rabrodbeck <rbrodbeck@gmail.com>
2026-05-24 09:43:56 -0500

    refactor: change subprocess execution to native import

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-# main.py (At the root of your repository)
-import subprocess
-import sys
-import os
-
-if __name__ == "__main__":
-    # Absolute path safety fallback
-    script_path = os.path.join("src", "app.py")
-    
-    # Explicitly call the native streamlit binary using the current python environment
-    sys.exit(
-        subprocess.run([sys.executable, "-m", "streamlit", "run", script_path])
-    )
+# main.py
+import src.app
