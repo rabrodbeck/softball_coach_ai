38eb52a559e07b12214c0d7ad5d7da9eebbee32d
rabrodbeck <rbrodbeck@gmail.com>
2026-07-16 14:17:31 -0500

    feat(db): add search_players_global query function

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -22,6 +22,7 @@ from .players import (
     add_player,
     add_returning_player,
     get_team_players,
+    search_global_players,
     get_coach_players_directory,
     update_player_stats,
     delete_player,
